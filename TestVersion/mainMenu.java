2b7c89d221bff81bb5cffab8c540b37d823f68fc
Healsu <timmie2500@gmail.com>
2021-11-22 10:50:03 +0100

    Added Packages to better organaize the files

--- TestVersion/mainMenu.java
+++ TestVersion/mainMenu.java
@@ -1,26 +1,26 @@
 import Boss.Boss;
 import Customer.Customer;
-
 import java.util.Scanner;
 
 public class mainMenu {
     public static void main(String[] args) {
+        boolean menu = true;
         Scanner scanner = new Scanner(System.in);
         //Cant call the method withouth the object, needs to be changed
-        Customer newCustomer = new Customer("",0);
+        Customer newCustomer = new Customer("TestUser",0);
         Boss boss = new Boss("Timmie >:)");
-        System.out.println("Choose an option: \n1. Create new user \n2. Boss.Boss Menu \n3. Price Rules \n4. Quit the program");
-        int userInput = scanner.nextInt();
+
+        while(menu == true){
+            System.out.println("Choose an option: \n1. Boss Menu \n2. Price Rules \n3. Quit the program");
+            int userInput = scanner.nextInt();
         switch (userInput){
             case 1:
-
+                boss.bossMenu(newCustomer);
             case 2:
-                System.out.println("Welcome "+boss.bossName+" what would you like to see?");
 
             case 3:
-
-            case 4:
-                break;
+              menu = false;
+        }
         }
     }
 
