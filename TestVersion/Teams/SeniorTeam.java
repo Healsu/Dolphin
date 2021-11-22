e47ffac0b97d6a15300fcd87374e9fb8422d8bcc
siswanto@live.dk <Hfg93rdqhfg>
2021-11-22 12:14:42 +0100

    added SeniorTeam.java and YouthTeam.java

---
+++ TestVersion/Teams/SeniorTeam.java
@@ -0,0 +1,11 @@
+package Teams;
+
+import Customer.Customer;
+
+import java.util.ArrayList;
+
+public class SeniorTeam extends Team{
+    public SeniorTeam(ArrayList<Customer> teamMembers) {
+        super(teamMembers);
+    }
+}
--- TestVersion/Teams/Team.java
+++ TestVersion/Teams/Team.java
@@ -4,33 +4,17 @@ import Customer.Customer;
 import java.util.ArrayList;
 
 public class Team {
-    private String swimDisciplin;
-    private String ageCategory;
     private ArrayList<Customer> teamMembers;
 
-
-    private int ageGroupThreshold = 18;
-
-    public void ageGroupIdentifier(){
-        int ageGroup = this.ageGroupThreshold;
-        if(ageGroup >= ageGroupThreshold){
-            System.out.println("Over 18 group");
-        } else {
-            System.out.println("Under 18 group");
-        }
+    public Team(ArrayList<Customer> teamMembers) {
+        this.teamMembers = teamMembers;
     }
 
     public void addToTeam(Customer swimmer){
-        if (swimmer.getUserAge() >= ageGroupThreshold){
-            teamMembers.add(swimmer);
-        } {
-            teamMembers.add(swimmer);
-        }
+        teamMembers.add(swimmer);
     }
 
     public void checkTeam(){
-        System.out.println("Disciplin: " +swimDisciplin);
-        System.out.println("Age group: " + ageCategory);
         for (Customer member: teamMembers ) {
             System.out.println("Name: " + member.getUserName());
             System.out.println("Age: " + member.getUserAge());
