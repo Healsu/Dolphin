50247640a6501df90e4dd94120592ef547b90802
siswanto@live.dk <Hfg93rdqhfg>
2021-11-22 11:25:38 +0100

    added checkTeam

--- TestVersion/Teams/Team.java
+++ TestVersion/Teams/Team.java
@@ -5,27 +5,35 @@ import java.util.ArrayList;
 
 public class Team {
     private String swimDisciplin;
-    private int ageGroup;
+    private String ageCategory;
     private ArrayList<Customer> teamMembers;
 
 
-    private int adultAge = 18;
-
+    private int ageGroupThreshold = 18;
 
     public void ageGroupIdentifier(){
-        int ageGroup = this.ageGroup;
-        if(ageGroup >= adultAge){
+        int ageGroup = this.ageGroupThreshold;
+        if(ageGroup >= ageGroupThreshold){
             System.out.println("Over 18 group");
         } else {
             System.out.println("Under 18 group");
         }
     }
 
-    public void addToTeam(Customer userAge){
-        if (userAge.getUserAge() >= adultAge){
-
+    public void addToTeam(Customer swimmer){
+        if (swimmer.getUserAge() >= ageGroupThreshold){
+            teamMembers.add(swimmer);
         } {
+            teamMembers.add(swimmer);
+        }
+    }
 
+    public void checkTeam(){
+        System.out.println("Disciplin: " +swimDisciplin);
+        System.out.println("Age group: " + ageCategory);
+        for (Customer member: teamMembers ) {
+            System.out.println("Name: " + member.getUserName());
+            System.out.println("Age: " + member.getUserAge());
         }
     }
 }
